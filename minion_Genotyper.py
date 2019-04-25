d94fd919a9ec052ab846c67afdec362cfa196384
Salvo Camiolo <scc20x@Alpha.cvr.gla.ac.uk>
2019-04-25 13:22:55 +0100

    kmer dataset added

--- minion_Genotyper.py
+++ minion_Genotyper.py
@@ -6,6 +6,7 @@ minimumReadLength = int(sys.argv[2])
 
 print "Filtering sequences longer than",minimumReadLength,"nucleotides"
 numSeq= 0
+foundSequences = 0
 sequences = {}
 for seq_record in SeqIO.parse(readFile,"fastq"):
     numSeq+=1
@@ -13,9 +14,10 @@ for seq_record in SeqIO.parse(readFile,"fastq"):
         print numSeq,"reads have been tested...."
     if len(str(seq_record.seq)) > minimumReadLength:
         if not str(seq_record.id) in sequences:
+            foundSequences +=1
             sequences[str(seq_record.id)] = str(seq_record.seq)
 
-
+print "\n",foundSequences,"reads longer than 50000 nucleotides were found...."
 
 
 
